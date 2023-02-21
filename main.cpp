f8beeb1b94a7a37b88e639f7be65fdeb1ed5659c
sheblaev <sheblaev@gmail.com>
2023-02-21 09:25:47 +0300

    The first stage of knapsack prototype

---
+++ main.cpp
@@ -0,0 +1,51 @@
+class Conditions {};
+class Knapsack {} ;
+class Solver {} ;
+
+// read conditions
+// 	class:
+// 		Conditions
+// 			Conditions - ctor(const vector<> & , const vector<> &, int)
+// 		Knapsack
+// solve the problem
+// 	class:
+// 		Solver
+// 			ctor(const Condition& )
+// 			void solve()
+// 			int answer()
+// 			const vector<int>&  solution()
+// print results
+// 	
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	vector<int> v; 
+	vector<int> w;
+	int K;
+	int n;
+	
+	cin >> n;
+	cin >> k;
+	v.resize(n);
+	w.resize(n);
+	for (int i = 0; i < n; i++) {
+		cin >> v[i];
+		cin >> w[i];
+	}
+
+	Conditions the_cond(v, w, K);
+	Solver the_solver(cond);
+	the_solver.solve();
+
+	vector<int>& solution = the_solver.solution();
+	cout << the_solver.answer() << 0 << endl;
+	for (int i = 0; i < n; i++) {
+		cout << solution[i] << endl;
+	}
+
+
+	return 0;
+}
